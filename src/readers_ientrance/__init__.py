6d54a3302e69e1713b82b608206402c8de0b7d15
Abbas Gholami <abbas.gholami71@gmail.com>
2026-04-21 10:23:38 +0200

    add robust state-machine reader for MicroMag AGM txt files

--- src/readers_ientrance/__init__.py
+++ src/readers_ientrance/__init__.py
@@ -1,3 +1,8 @@
 from .vsm_reader import read_lakeshore_vsm, VSMData
+from .agm_reader import read_micromag_agm
 
-__all__ = ["read_lakeshore_vsm", "VSMData"]
+__all__ = [
+    "read_lakeshore_vsm", 
+    "VSMData",
+    "read_micromag_agm"
+]
