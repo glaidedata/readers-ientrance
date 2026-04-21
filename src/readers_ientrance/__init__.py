1db73d102e97c5e7cd50024522d98af76f7c61c2
Abbas Gholami <abbas.gholami71@gmail.com>
2026-04-21 11:42:29 +0200

    ruff

--- src/readers_ientrance/__init__.py
+++ src/readers_ientrance/__init__.py
@@ -1,8 +1,4 @@
 from .vsm_reader import read_lakeshore_vsm, VSMData
 from .agm_reader import read_micromag_agm
 
-__all__ = [
-    "read_lakeshore_vsm", 
-    "VSMData",
-    "read_micromag_agm"
-]
+__all__ = ["read_lakeshore_vsm", "VSMData", "read_micromag_agm"]
