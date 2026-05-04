f7dce77a7ceb9b973363f09e021d9436cdec2855
Abbas Gholami <abbas.gholami71@gmail.com>
2026-05-04 13:45:23 +0200

    build: update init exports and add kaitaistruct dependency

--- src/readers_ientrance/__init__.py
+++ src/readers_ientrance/__init__.py
@@ -1,4 +1,12 @@
 from .vsm_reader import read_lakeshore_vsm, VSMData
 from .agm_reader import read_micromag_agm, AGMData
+from .ntmdt_reader import read_ntmdt, NTMDTAFMData
 
-__all__ = ["read_lakeshore_vsm", "VSMData", "read_micromag_agm", "AGMData"]
+__all__ = [
+    "read_lakeshore_vsm", 
+    "VSMData", 
+    "read_micromag_agm", 
+    "AGMData",
+    "read_ntmdt",
+    "NTMDTAFMData"
+]
